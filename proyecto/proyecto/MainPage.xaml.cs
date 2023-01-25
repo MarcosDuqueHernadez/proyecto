e51f2980235d3e2627180a0c7055bbb623b7583b
midh <midh@DEV>
2023-01-25 04:35:05 -0600

    Agregar archivos de proyecto.

---
+++ proyecto/proyecto/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace proyecto
+{
+    public partial class MainPage : ContentPage
+    {
+        //public IList<grid> dataGrids { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public class Card
+        {
+            public string Id { get; set; }
+            public string nombre { get; set; }
+        }
+    }
+}
